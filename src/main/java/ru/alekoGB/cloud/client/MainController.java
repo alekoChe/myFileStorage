6253f1c4c35812849fc29a4662e61a58a905c4cf
alekoChe <aleko2117bis@gmail.com>
2022-03-17 21:44:04 +0300

    Initial commit

---
+++ src/main/java/ru/alekoGB/cloud/client/MainController.java
@@ -0,0 +1,24 @@
+package ru.alekoGB.cloud.client;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+
+    public void downLoad(ActionEvent actionEvent) {
+    }
+
+    public void upLoad(ActionEvent actionEvent) {
+    }
+
+    public void createFolder(ActionEvent actionEvent) {
+    }
+
+    public void synchronizeFileView(ActionEvent actionEvent) {
+    }
+
+    public void storageView(ActionEvent actionEvent) {
+    }
+
+    public void myFileView(ActionEvent actionEvent) {
+    }
+}
